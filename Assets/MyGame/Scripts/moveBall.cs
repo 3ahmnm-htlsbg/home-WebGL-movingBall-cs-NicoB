b65e2a5a91a522ea60378d51dc36e1605c77c83e
NicoB <nicolas.bachleitner03@htl-salzburg.ac.at>
2020-01-13 08:16:44 +0100

    add unclean written script - but it works

---
+++ Assets/MyGame/Scripts/moveBall.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveBall : MonoBehaviour
+{
+	[SerializeField]  private GameObject[] ballPositions; 
+
+	public GameObject BallGO;
+
+	int randomInt;
+	int randomIntBefore;
+
+	public void SetBallPos()
+	{
+		randomInt = Random.Range(0 ,4);
+		if (randomInt != randomIntBefore)
+		{
+			randomIntBefore = randomInt;
+			BallGO.transform.position = ballPositions[randomInt].transform.position;
+		} else 
+		{
+			SetBallPos();
+		}
+	}
+}
